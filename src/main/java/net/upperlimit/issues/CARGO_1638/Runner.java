8ce16b376a854ef9411054df820df63636979a53
eliasbalasis <eliasbalasis@gmail.com>
2025-03-14 12:38:09 +0000

    runner

---
+++ src/main/java/net/upperlimit/issues/CARGO_1638/Runner.java
@@ -0,0 +1,47 @@
+package net.upperlimit.issues.CARGO_1638;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.codehaus.cargo.container.jetty.Jetty12xEmbeddedLocalContainer;
+import org.codehaus.cargo.container.jetty.Jetty12xEmbeddedStandaloneLocalConfiguration;
+import org.codehaus.cargo.container.jetty.JettyPropertySet;
+import org.codehaus.cargo.container.property.ServletPropertySet;
+import org.codehaus.cargo.util.log.LogLevel;
+import org.codehaus.cargo.util.log.Logger;
+import org.codehaus.cargo.util.log.SimpleLogger;
+
+public class Runner {
+
+	public static void main(String[] args) throws Exception {
+
+		final Path dir = Files.createTempDirectory("upperlimit-issues-CARGO-1638-");
+		dir.toFile().deleteOnExit();
+
+		final Jetty12xEmbeddedStandaloneLocalConfiguration configuration = //
+				new Jetty12xEmbeddedStandaloneLocalConfiguration( //
+						dir.toString() //
+				);
+		configuration.setProperty( //
+				ServletPropertySet.PORT, //
+				String.valueOf(8888) //
+		);
+		configuration.setProperty( //
+				JettyPropertySet.USE_FILE_MAPPED_BUFFER, //
+				String.valueOf(false) //
+		);
+		configuration.setProperty( //
+				JettyPropertySet.DEPLOYER_EE_VERSION, //
+				"ee10" //
+		);
+
+		final Jetty12xEmbeddedLocalContainer container = //
+				new Jetty12xEmbeddedLocalContainer( //
+						configuration //
+				);
+		final Logger containerLogger = new SimpleLogger();
+		containerLogger.setLevel(LogLevel.DEBUG);
+		container.setLogger(containerLogger);
+		container.start();
+	}
+}
